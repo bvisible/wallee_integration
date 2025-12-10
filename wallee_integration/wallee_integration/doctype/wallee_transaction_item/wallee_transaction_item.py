5a21def8652483e9b7e7aa8c55ef8bc26ae96fb7
Jérémy Christillin <jeremy@neoservice.ai>
2025-12-10 13:23:36 +0100

    Add missing wallee_transaction_item.py file

---
+++ wallee_integration/wallee_integration/doctype/wallee_transaction_item/wallee_transaction_item.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2024, Neoservice and contributors
+# For license information, please see license.txt
+
+from frappe.model.document import Document
+
+
+class WalleeTransactionItem(Document):
+    """Wallee Transaction Item - Child table for transaction line items."""
+
+    pass
